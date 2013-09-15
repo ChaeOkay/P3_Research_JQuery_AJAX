bcccb6caa30f5cc5f087c1c523fb5beb65ef7b7c
cokeefe <chaemobile@gmail.com>
2013-09-15 13:06:52 -0700

    instance to local variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ActionController::Base
   end
 
   def grandma
-    @response = params[:user_input]
-    @msg = @response == @response.upcase ? "NOT SINCE 1983" : "SPEAK UP SONNY"
+    response = params[:user_input]
+    @msg = response == response.upcase ? "NOT SINCE 1983" : "SPEAK UP SONNY"
   end
 end
