6effef5c0c14d959b69ddcb95c2ae717c7a087b5
Apprentice <apprentice@dbc24.local>
2013-09-16 14:43:42 -0700

    file cleanup add respond_to comment in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ActionController::Base
   def show
     response = params[:user_input]
     @msg = response == response.upcase ? "NOT SINCE 1983" : "SPEAK UP SONNY"
+
+    # respond_to do |format|
+    #   format.html {}
+    #   format.js {}
+    # end
   end
 end
