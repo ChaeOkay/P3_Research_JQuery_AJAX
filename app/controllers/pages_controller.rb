9d490c83bfea85e0f715323ee689a7107f84815d
cokeefe <chaemobile@gmail.com>
2013-09-15 19:33:41 -0700

    config routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,10 @@ class PagesController < ActionController::Base
   def show
     response = params[:user_input]
     @msg = response == response.upcase ? "NOT SINCE 1983" : "SPEAK UP SONNY"
-    render "show"
+
+    respond_to do |format|
+      format.html { render layout: "show"}
+      format.js
+    end
   end
 end
